5a571cc172c0b8dc0538dc5f4ec5ee2faaac908c
Barret J. Nobel <bear.nobel@gmail.com>
2016-06-08 16:34:19 -0400

    created penguin class added Model group and put penguin inside

---
+++ TripToTheZoo/Penguin.swift
@@ -0,0 +1,17 @@
+//
+//  Penguin.swift
+//  TripToTheZoo
+//
+//  Created by Mr. Nobel on 6/8/16.
+//  Copyright © 2016 Mr. Nobel. All rights reserved.
+//
+
+import Foundation
+import UIKit //... needed for UIImage
+
+struct Penguin {
+    var age: Int
+    var name: String
+    var species: String
+    var image: UIImage
+}
